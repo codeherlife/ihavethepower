c2c53beb68fef17bd1591f9370d3d9fe67f63fc2
codeherlife <codeherlife@gmail.com>
2018-08-27 21:22:59 -0700

    Initial Commit

---
+++ MyApp/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MyApp
+//
+//  Created by Melanie Mills on 8/27/18.
+//  Copyright © 2018 CSCI. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
