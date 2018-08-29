d5bb7ca824007068027f53e9e443fd07da2afd7d
codeherlife <codeherlife@gmail.com>
2018-08-28 17:16:22 -0700

    added features in order to send the output to a text file

--- MyApp/main.c
+++ MyApp/main.c
@@ -7,17 +7,26 @@
 //
 
 #include <stdio.h>
+
+FILE *fp;
+
 #define MIN 1
 #define MAX 30
 
 int main(void) {
     int i;
     
+    fp = fopen("csis.txt", "w");
+    
     printf("%8s %8s %8s %8s %8s\n", "Value", "Square", "Cube", "Fourth", "Fifth");
+    fprintf(fp, "%8s %8s %8s %8s %8s\n", "Value", "Square", "Cube", "Fourth", "Fifth");
     printf("%8s %8s %8s %8s %8s\n", "-----", "------", "----", "------", "-----");
+    fprintf(fp, "%8s %8s %8s %8s %8s\n", "-----", "------", "----", "------", "-----");
     
     for (i = MIN; i <= MAX; ++i){
         printf("%8d %8d %8d %8d %8d\n", i, i * i, i * i * i, i * i * i * i, i * i * i * i * i);
+        fprintf(fp, "%8d %8d %8d %8d %8d\n", i, i * i, i * i * i, i * i * i * i, i * i * i * i * i);
     }
+    fclose(fp);
     return 0;
 }
